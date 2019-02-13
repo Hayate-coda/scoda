561e880549c247ff5259e339e2a178caf3739b69
Hayate Imori <imoriii0925bass@gmail.com>
2019-02-13 09:06:38 +0900

    <ランキングのマウスオーバー実装>

--- js/original.js
+++ js/original.js
@@ -9,12 +9,15 @@
 });
 
 $('#zoommini').on('click', function() {
-  $("#sidebar").removeClass("d-none");
-$("#sidebar").addClass("col-12 lead");
-
+  $("#sidebar,#joinbutton").removeClass("d-none");
+$("#sidebar,#joinbutton").addClass("col-12 lead mr-auto ml-auto");
 });
 
 $('#minisearch').on('click', function() {
 $("#search").toggleClass("d-none col-12 p-0");
 $("#searchinput").toggleClass("w-100");
+});
+
+$('.dropdown').hover(function() {
+$(".dropdown-menu").toggleClass("show");
 });
